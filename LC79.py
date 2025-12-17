90bd85bdb8a10e32694494ce44863acc92796d6b
Samar <reddysamar55@gmail.com>
2025-12-17 22:15:08 +0530

    med

---
+++ LC79.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def exist(self, board, word):
+        rows, cols = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return False
+            if board[r][c] != word[i] or (r, c) in path:
+                return False
+            path.add((r, c))
+            found = (
+                dfs(r + 1, c, i + 1) or
+                dfs(r - 1, c, i + 1) or
+                dfs(r, c + 1, i + 1) or
+                dfs(r, c - 1, i + 1)
+            )
+            path.remove((r, c))
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
+
+
+if __name__ == "__main__":
+    board = [
+        ["A", "B", "C", "E"],
+        ["S", "F", "C", "S"],
+        ["A", "D", "E", "E"]
+    ]
+    sol = Solution()
+    print(sol.exist(board, "ABCCED"))
+    print(sol.exist(board, "SEE"))
+    print(sol.exist(board, "ABCB"))
