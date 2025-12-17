a7b3d2991023ac78e52131f3aa0f8662be836233
Samar <reddysamar55@gmail.com>
2025-12-17 22:47:25 +0530

    med

---
+++ LC230.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root, k):
+        n = 0
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
+
+
+def insert(root, val):
+    if not root:
+        return TreeNode(val)
+    if val < root.val:
+        root.left = insert(root.left, val)
+    else:
+        root.right = insert(root.right, val)
+    return root
+
+
+if __name__ == "__main__":
+    vals = [5, 3, 6, 2, 4, 1]
+    root = None
+    for v in vals:
+        root = insert(root, v)
+
+    sol = Solution()
+    print(sol.kthSmallest(root, 3))
