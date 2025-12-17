241e74579b6b720460fca764514e11dcdc478efd
Samar <reddysamar55@gmail.com>
2025-12-17 22:06:54 +0530

    med

---
+++ LC701.py
@@ -0,0 +1,34 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        if not root:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+
+def inorder(root):
+    if not root:
+        return []
+    return inorder(root.left) + [root.val] + inorder(root.right)
+
+
+if __name__ == "__main__":
+    root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+
+    sol = Solution()
+    root = sol.insertIntoBST(root, 5)
+    print(inorder(root))
