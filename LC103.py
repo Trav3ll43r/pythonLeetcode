db2884657d1feaf7a48207ccc616ae102b45d920
Samar <reddysamar55@gmail.com>
2025-12-17 22:45:43 +0530

    med

---
+++ LC103.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        res = []
+        q = deque([root] if root else [])
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if len(res) % 2 == 1:
+                level.reverse()
+            res.append(level)
+        return res
+
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    sol = Solution()
+    print(sol.zigzagLevelOrder(root))
